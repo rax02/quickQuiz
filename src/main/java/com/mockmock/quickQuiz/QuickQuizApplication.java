700082d4133567a363534e36a99af7a8492fcb2e
Ravi Maurya <0001rkm@gmail.com>
2023-08-03 00:48:08 +0530

    initial spring project

---
+++ src/main/java/com/mockmock/quickQuiz/QuickQuizApplication.java
@@ -0,0 +1,13 @@
+package com.mockmock.quickQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockmock/quickQuiz/QuickQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockmock.quickQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
